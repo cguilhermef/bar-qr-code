91ca62672e55d5cef3b829a15a64c5fda37c3f66
Christian G. Fritsch <christian@checklistfacil.com.br>
2021-05-28 10:37:21 -0300

    feat: change approach for methods to allow function on jquery 'on'

--- script.js
+++ script.js
@@ -5,71 +5,73 @@ window.addEventListener("load", () => {
     let selectedDeviceId = null;
     let currentFieldTarget = null;
 
+    const methods = {
+      showOverlay: () => {
+        $("#bqc-overlay").css("display", "flex");
+      },
+      hideOverlay: () => {
+        codeReader.reset();
+        codeReader.stopContinuousDecode();
+        codeReader = null;
+        currentFieldTarget = null;
+        $("#bqc-overlay").css("display", "none");
+      },
+      setValueOnTarget: (target, val) => {
+        $(`input[data-pkitem="${target}"]`).val(val);
+      },
+      start: (fieldTarget) => {
+        if (codeReader) {
+          codeReader.reset();
+          codeReader = null;
+        }
+        codeReader = new ZXing.BrowserMultiFormatReader();
+        codeReader.listVideoInputDevices().then((videoInputDevices) => {
+          selectedDeviceId = videoInputDevices[selectedDevice].deviceId;
+
+          if (videoInputDevices.length > 1) {
+            $("#bqc-btn-change").show();
+          } else {
+            $("#bqc-btn-change").hide();
+          }
+          codeReader.decodeFromVideoDevice(
+            selectedDeviceId,
+            "video",
+            (result, err) => {
+              if (result) {
+                methods.setValueOnTarget(fieldTarget, result.text);
+                methods.hideOverlay();
+              }
+              if (err && !(err instanceof ZXing.NotFoundException)) {
+                methods.hideOverlay();
+              }
+            }
+          );
+        });
+      },
+    };
+
     // event listeners
-    $(document).on("click", ".js-bqc-button-read", (el) => {
-      currentFieldTarget = $(el.target).attr("data-target-item");
-      showOverlay();
-      start(currentFieldTarget);
+    $(document).on("click", ".js-bqc-button-read", function () {
+      currentFieldTarget = $(this).attr("data-target-item");
+      console.log(currentFieldTarget);
+      methods.showOverlay();
+      methods.start(currentFieldTarget);
     });
 
     $(document).on("click", "#bqc-btn-back", () => {
-      hideOverlay();
+      methods.hideOverlay();
     });
 
     $(document).on("click", "#bqc-btn-change", () => {
       selectedDevice = selectedDevice === 1 ? 0 : 1;
       codeReader.reset();
-      start(currentFieldTarget);
+      methods.start(currentFieldTarget);
     });
 
     $(document).on("click", "#bqc-btn-close", () => {
-      hideOverlay();
+      methods.hideOverlay();
     });
 
     // methods
-    const showOverlay = () => {
-      $("#bqc-overlay").css("display", "flex");
-    };
-    const hideOverlay = () => {
-      codeReader.reset();
-      codeReader.stopContinuousDecode();
-      codeReader = null;
-      currentFieldTarget = null;
-      $("#bqc-overlay").css("display", "none");
-    };
-
-    const setValueOnTarget = (target, val) => {
-      $(`input[data-pkitem="${target}"]`).val(val);
-    };
-
-    const start = (fieldTarget) => {
-      if (codeReader) {
-        codeReader.reset();
-        codeReader = null;
-      }
-      codeReader = new ZXing.BrowserMultiFormatReader();
-      codeReader.listVideoInputDevices().then((videoInputDevices) => {
-        selectedDeviceId = videoInputDevices[selectedDevice].deviceId;
-
-        if (videoInputDevices.length > 1) {
-          $("#bqc-btn-change").show();
-        } else {
-          $("#bqc-btn-change").hide();
-        }
-        codeReader.decodeFromVideoDevice(
-          selectedDeviceId,
-          "video",
-          (result, err) => {
-            if (result) {
-              setValueOnTarget(fieldTarget, result.text);
-              hideOverlay();
-            }
-            if (err && !(err instanceof ZXing.NotFoundException)) {
-              hideOverlay();
-            }
-          }
-        );
-      });
-    };
   })();
 });
